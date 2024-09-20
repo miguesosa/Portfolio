6e6084544cc598d97e885fee0ca6e7a03432e2ff
miguesosa <migueladriansosa@gmail.com>
2024-09-19 23:01:47 -0300

    Agregar fondo dinámico y efectos de hover en botones; mejorar la estructura y accesibilidad del portfolio para una experiencia de usuario más atractiva y responsiva.

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Añadir clase "loaded" al body cuando la página esté completamente cargada
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.classList.add("loaded");
+});

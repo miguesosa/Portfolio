6c0df1f96127faec5846602ded006490507e7839
miguesosa <migueladriansosa@gmail.com>
2024-10-12 00:30:36 -0300

    Ajuste e implementación de animaciones

--- js/main.js
+++ js/main.js
@@ -29,3 +29,54 @@ function removeDarkTheme() {
         darkTheme.remove();  // Elimina el enlace del head
     }
 }
+
+// Función para verificar si un elemento está en la vista
+const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+  
+  // Animar elementos al desplazarse
+  const animateOnScroll = () => {
+    const headers = document.querySelectorAll('#habilidades h4');
+    const cols = document.querySelectorAll('#habilidades .col-md-12');
+  
+    headers.forEach((header) => {
+      if (isElementInViewport(header)) {
+        header.classList.add('fade-in-left');
+      }
+    });
+  
+    cols.forEach((col) => {
+      if (isElementInViewport(col)) {
+        col.classList.add('fade-in-bottom');
+      }
+    });
+  };
+  
+  // Detectar el desplazamiento
+  window.addEventListener('scroll', animateOnScroll);
+
+  // Función para observar elementos
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show'); // Esto permite que se repita la animación al hacer scroll
+      }
+    });
+  });
+  
+  // Selecciona el h4 y las columnas para observarlos
+  const fadeInLeftElements = document.querySelectorAll('.fade-in-left');
+  const fadeInBottomElements = document.querySelectorAll('.fade-in-bottom');
+  
+  // Observa los elementos para activar la animación
+  fadeInLeftElements.forEach(el => observer.observe(el));
+  fadeInBottomElements.forEach(el => observer.observe(el));
